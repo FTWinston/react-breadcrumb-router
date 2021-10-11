6b97039b8db0f0452b863800d588841d1a41470f
FTWinston <ftwinston@gmail.com>
2021-10-11 21:45:47 +0100

    Props are now exported using separate "export type" statements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-export { Breadcrumb, Props as BreadcrumbProps } from './Breadcrumb';
-export { Breadcrumbs, Props as BreadcrumbsProps } from './Breadcrumbs';
-export { BreadcrumbRoute, Props as BreadcrumbRouteProps } from './BreadcrumbRoute';
+export { Breadcrumb } from './Breadcrumb';
+export { Breadcrumbs } from './Breadcrumbs';
+export { BreadcrumbRoute } from './BreadcrumbRoute';
 export { BreadcrumbRouter } from './BreadcrumbRouter'
-export { Title, Props as TitleProps } from './Title';
+export { Title } from './Title';
+
+export type { Props as BreadcrumbProps } from './Breadcrumb';
+export type { Props as BreadcrumbsProps } from './Breadcrumbs';
+export type { Props as BreadcrumbRouteProps } from './BreadcrumbRoute';
+export type { Props as TitleProps } from './Title';
